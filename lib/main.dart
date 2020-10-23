7b5d2f093a20601c6e71c6877aebde3d6dbfe815
Aek Sae-khow <saekhow.aek@gmail.com>
2020-10-23 19:53:36 +0700

    46. How to use Column & Row Widgets for Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 100,
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Text('Hello 1'),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Text('Hello 2'),
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Text('Hello 3'),
+                ),
+                Container(
+                  width: double.infinity,
+                ),
+              ],
+            ),
+          )),
+    );
+  }
 }
